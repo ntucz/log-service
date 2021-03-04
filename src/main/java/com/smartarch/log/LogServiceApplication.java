f1bdd7ecdf07a589a3ecf106aa6a6f1bcd34c653
ntucz <chenzheng6809@163.com>
2021-03-04 23:05:33 +0800

    update log-service

--- src/main/java/com/smartarch/log/LogServiceApplication.java
+++ src/main/java/com/smartarch/log/LogServiceApplication.java
@@ -1,29 +1,14 @@
 package com.smartarch.log;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class LogServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LogServiceApplication.class, args);
-    }
-
-    @RestController
-    static class HelloController {
-
-        @Value("${name:}")
-        private String name;
-
-        @RequestMapping("/")
-        public String index() {
-            return name;
-        }
-
     }
 
 }
