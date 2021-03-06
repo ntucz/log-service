ba3e17b3e1034f5c3b72a4f51dc04c405f0b22fd
ntucz <chenzheng6809@163.com>
2021-03-06 18:01:13 +0800

    update es

--- src/main/java/com/smartarch/log/controller/ElasticController.java
+++ src/main/java/com/smartarch/log/controller/ElasticController.java
@@ -2,6 +2,7 @@ package com.smartarch.log.controller;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,23 +21,26 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/logs")
 public class ElasticController {
 
-  @Autowired
-  private IElasticService elasticService;
-  
-  @GetMapping("/init")
-  public void init(){
-      elasticService.createIndex();
-      List<LogMessage> list =new ArrayList<>();
-      list.add(new LogMessage(1L, IConst.LOG_TYPE_OPER, "user_service", IConst.LOG_RANK_NOTICE, "admin", new Date(), "testesdfsfsdfsf"));
-      list.add(new LogMessage(2L, IConst.LOG_TYPE_OPER, "user_service", IConst.LOG_RANK_WARN, "ccc", new Date(), "teste1211sdfsfsdfsf"));
-      list.add(new LogMessage(3L, IConst.LOG_TYPE_SYS, "user_service", IConst.LOG_RANK_ERROR, "zzz", new Date(), "testes22222dfsfsdfsf"));
-      elasticService.saveAll(list);
+	@Autowired
+	private IElasticService elasticService;
 
-  }
+	@GetMapping("/init")
+	public void init() {
+		elasticService.createIndex();
+		List<LogMessage> list = new ArrayList<>();
+		list.add(new LogMessage(1L, IConst.LOG_TYPE_OPER, "user_service", IConst.LOG_RANK_NOTICE, "admin", new Date(),
+				"testesdfsfsdfsf"));
+		list.add(new LogMessage(2L, IConst.LOG_TYPE_OPER, "user_service", IConst.LOG_RANK_WARN, "ccc", new Date(),
+				"teste1211sdfsfsdfsf"));
+		list.add(new LogMessage(3L, IConst.LOG_TYPE_SYS, "user_service", IConst.LOG_RANK_ERROR, "zzz", new Date(),
+				"testes22222dfsfsdfsf"));
+		elasticService.saveAll(list);
 
-	/*
-	 * @GetMapping("/all") public Iterator<LogMessage> all(){ return
-	 * elasticService.findAll(); }
-	 */
+	}
+
+	@GetMapping("/all")
+	public Iterator<LogMessage> all() {
+		return elasticService.findAll();
+	}
 
 }
